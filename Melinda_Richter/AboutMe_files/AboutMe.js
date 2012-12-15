6dab244780d11b32155a546d75402deb7b05c17e
secretrobotron <secretrobotron@gmail.com>
2012-12-15 10:29:36 -0500

    initial content

---
+++ Melinda_Richter/AboutMe_files/AboutMe.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20121212
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('AboutMe_files/Crayon_BK_v4%20%2801%29.png'),IWCreateImage('AboutMe_files/Crayon_BK_v4%20%2802%29.png'),IWCreateImage('AboutMe_files/Crayon_BK_v4%20%2803%29.png'),IWCreateImage('AboutMe_files/Crayon_BK_v4%20%2805%29.png'),IWCreateImage('AboutMe_files/Crayon_BK_v4%20%2808%29.png'),IWCreateImage('AboutMe_files/Crayon_BK_v4%20%2807%29.png'),IWCreateImage('AboutMe_files/Crayon_BK_v4%20%2806%29.png'),IWCreateImage('AboutMe_files/Crayon_BK_v4%20%2804%29.png')],null,0,0.700000,19.000000,19.000000,19.000000,19.000000,41.000000,41.000000,41.000000,41.000000,2.000000,1.000000,2.000000,1.000000,null,null,null,0.100000)});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('AboutMe_files/AboutMeMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
---
+++ Melinda_Richter/PreviousProjects_files/PreviousProjects.js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.4 local-build-20121212
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('PreviousProjects_files/PreviousProjectsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
